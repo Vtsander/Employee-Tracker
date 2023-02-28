0f6cc477a44a9b9cc543668a0dfa73b37b624444
Veroneca Sander <nguyenveroneca@yahoo.com>
2023-02-28 15:13:18 -0800

    adding javascript

--- Develop/index.js
+++ Develop/index.js
@@ -4,3 +4,21 @@ const { exit } = require('process');
 const pass = require('../../pass/pass')
 const cTable = require('console.table')
 
+const db = mysql.createConnection(
+  {
+      host: 'localhost',
+      user: 'root',
+      password: pass,
+      database: 'employees_db'
+  },
+  console.log('Connected to the employees_db database')
+);
+
+const initPrompt = [{
+  type: "list",
+  name: "initAction",
+  message: "what would you like to do?",
+  choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role', 'exit application']
+}]
+
+init();
