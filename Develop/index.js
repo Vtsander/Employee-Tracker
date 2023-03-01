5df9988e6a86b6f80d450a603f87f509dfc7dc95
Veroneca Sander <nguyenveroneca@yahoo.com>
2023-02-28 16:01:45 -0800

    adding update portion

--- Develop/index.js
+++ Develop/index.js
@@ -57,4 +57,78 @@ const roleQuery = async(data) => {
   })
 }
 
+const empQuery = async(data, data2) => {
+  const res = await iq.prompt([{
+      type: "input",
+      name: "empfName",
+      message: "What is the employee's first name?"
+  },{
+      type: "input",
+      name: "emplName",
+      message: "What is the employee's last name?" 
+  },{
+      type: "list",
+      name: "empRole",
+      message: "What is the employee's role?",
+      choices: data2
+      
+  },{
+      type: "list",
+      name: "empMan",
+      message: "Who is the employee's manager?",
+      choices: data
+  }])
+
+  var hasManager = data.find(element => {
+      return element.name === res.empMan
+  })
+
+  var hasRole = data2.find(el => {
+      return el.name === res.empRole
+  })
+  db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${res.empfName}", "${res.emplName}", ${hasRole.id}, ${hasManager.id})`, function (err, res) {
+      init()
+  })
+}
+
+const updateQuery = async (data, data2) => {
+
+ const res = await iq.prompt([{
+      type: 'list',
+      name: 'updateEmp',
+      message: 'Which employee would you like to update?',
+      choices: data
+  },{
+    type: 'list',
+    name:'updateEmpRole',
+    message: "What is the employee's new role?",
+    choices: data2
+    
+  }])
+
+  const empEl = data.find(el => {
+      return el.name === res.updateEmp 
+  })
+
+  const empElID = empEl.id
+
+  const roleEl = data2.find(el => {
+      return el.name === res.updateEmpRole
+  })
+
+  const roleElID = roleEl.id
+
+  const sql = `UPDATE employee SET role_id = ? WHERE id = ?`
+  const params = [roleElID, empElID]
+  db.query(sql, params, (err, result) => {
+      if (err) {
+          console.error(err)
+      } else {
+          init();
+      }
+  })
+
+ 
+}
+
 init();
