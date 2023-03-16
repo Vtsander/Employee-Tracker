05ed35423bd2646d9bc6d3c8d3fce7dc53b7e628
Veroneca Sander <nguyenveroneca@yahoo.com>
2023-03-15 23:33:44 -0700

    fixed add employee, started update employee

--- index.js
+++ index.js
@@ -47,6 +47,8 @@ const initPrompt = {
 
   const roleQuery = async (data) => {
     try {
+      const department = await db.promise().query(`SELECT * FROM department`);
+      const departmentNames = department[0].map(dept => dept.name);
       const userInput = await iq.prompt([
         { 
           type: "input", 
@@ -62,25 +64,33 @@ const initPrompt = {
           type: "list", 
           name: "depRole", 
           message: "What department does this role belong to?",
-          choices: data
+          choices: departmentNames
         }
       ]);
       
-      const selectedDepartment = data.find(department => department.name === userInput.depRole);
-      
-      db.query(
-        `INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)`,
-        [userInput.roleName, userInput.salary, selectedDepartment.id]
-      );
+      const selectedDepartment = department[0].find(dept => dept.name === userInput.depRole);
   
-      init();
+      db.query(
+        `INSERT INTO role (title, salary, department_id) VALUES ("${userInput.roleName}", "${userInput.salary}", ${selectedDepartment.id})`,
+        function (err, res) {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log('Role added successfully!');
+            init();
+          }
+        }
+      );
     } catch (error) {
       console.error(error);
     }
-  }
+  };
   
   const employQuery = async (employeeList, roleList) => {
     try {
+      const employee = await db.promise().query(`SELECT * FROM employee`);
+      const managerNames = employee[0].map(employee => employee.first_name + employee.last_name);
+      managerNames.push("");
       const employeeInfo = await iq.prompt([
         {
           type: "input",
@@ -96,31 +106,71 @@ const initPrompt = {
           type: "list",
           name: "roleId",
           message: "What is the employee's role?",
-          choices: roleList.map(role => role.name),
+          choices: roleList.map(role => role.title),
         },
         {
           type: "list",
           name: "managerId",
           message: "Who is the employee's manager?",
-          choices: employeeList.map(manager => manager.name),
+          choices: employeeList.map(employee => employee.first_name + employee.last_name),
         },
       ]);
-      
-      const selectedManager = employeeList.find(manager => manager.name === employeeInfo.managerId);
-      
-      const selectedRole = roleList.find(role => role.name === employeeInfo.roleId);
-      
-      const query = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${employeeInfo.firstName}", "${employeeInfo.lastName}", ${selectedRole.id}, ${selectedManager.id})`;
-      
-      db.query(query);
-      
-      init();
+  
+      const selectedEmployee = roleList.find(role => role.title === employeeInfo.roleId);
+
+      const selectedManager = employeeList.find(employee => employee.first_name + employee.last_name === employeeInfo.managerId);
+
+      db.query(
+        `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${employeeInfo.firstName}", "${employeeInfo.lastName}", ${selectedEmployee.id}, ${selectedManager.id})`,
+        function (err, res) {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log('Employee added successfully!');
+            init();
+          }       
+        }
+      );
     } catch (error) {
       console.error(error);
     }
   };
   
+ const updateQuery = async () => {
+  try {
+    const userUpdate = await iq.prompt([
+      { 
+        type: "list", 
+        name: "updateEmploy", 
+        message: "Which employee would you like to have updated?",
+        choices: employeeList.map(employee => employee.first_name + employee.last_name),
+      },
+      { 
+        type: "list", 
+        name: "UpdateRole", 
+        message: "What is the new role of this employee?",
+        choices:""
+      }
+    ]);
+
+      const selectedUpdateEmp = employeeList.find(employee => employee.first_name + employee.last_name === userUpdate.updateEmploy);
   
+      db.query(
+        `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${employeeInfo.firstName}", "${employeeInfo.lastName}", ${selectedEmployee.id}, ${selectedUpdateEmp.id})`,
+        function (err, res) {
+          if (err) {
+            console.error(err);
+          } else {
+            console.log('Employee updated successfully!');
+            init();
+          }       
+        }
+      );
+      } catch (error) {
+  console.error(error);
+  }
+ };
+ 
   const init = async() => {
     const res = await iq.prompt(initPrompt)
     if (res.initAction === 'View all departments'){
@@ -148,9 +198,20 @@ const initPrompt = {
         })
     }
     if (res.initAction === 'Add an employee'){
-        employQuery();
+        db.query('SELECT * FROM employee', function (err, employees){
+            db.query('SELECT * FROM role', function (err, roles){
+                employQuery(employees, roles);
+            });
+        });
     }
-    if (res.initAction === 'Exit Application'){
+    if (res.initAction === 'Update an employee role'){
+      db.query('SELECT * FROM employee', function (err, employees){
+        db.query('SELECT * FROM role', function (err, roles){
+            updateQuery(employees, roles);
+        });
+    });
+    }
+    if (res.initAction === 'Exit application'){
         process.exit(0);
     }
     return
